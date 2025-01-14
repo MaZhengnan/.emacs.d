c310f955fe4a8b19d7d20bebf3055ab761f1c329
mazhengnan <mzn836443465@163.com>
2025-01-14 15:38:22 +0800

    emacs from scratch

--- early-init.el
+++
@@ -1,6 +0,0 @@
-(setq package-enable-at-startup nil)
-
-(cond ((eq system-type 'windows-nt)
-  (setq elpaca-no-symlink-mode t))
-)
-
--- init.el
+++ init.el
@@ -1,11 +1,112 @@
-(require 'org)
+(setq inhibit-startup-message t)
 
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((emacs-lisp . t)))
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
 
-(org-babel-load-file
- (expand-file-name
-  "config.org"
-  user-emacs-directory))
+(menu-bar-mode -1)            ; Disable the menu bar
 
+;; Set up the visible bell
+(setq visible-bell t)
+
+(set-face-attribute 'default nil :font "FiraMono Nerd Font" :height 130)
+
+(load-theme 'wombat)
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package command-log-mode)
+
+(use-package ivy
+  ;;:ensure t
+  :demand t
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file 'noerror 'nomessage)
+
+(use-package restart-emacs)
+
+;; 绑定快捷键 C-c r
+(global-set-key (kbd "C-c r") 'restart-emacs)
+
+(defun open-init-file ()
+  "Open Emacs Configuration file quickly."
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))  ;; 用你实际的配置路径替换
+
+;; 例如绑定到 F2 键
+(global-set-key (kbd "<f2>") 'open-init-file)
+
+
+(use-package doom-themes
+  :init (load-theme 'doom-gruvbox))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 1))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history)))
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
